a59c944eb0bacbe41629c2ca37b3099e561b0850
Jerome <jerome.bonnet21@wanadoo.fr>
2016-05-03 10:22:34 +0200

    Tata commit
    add displayTata

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		displayToto();
+		displayTata();
 	}
 	
 	public static void displayToto() {
 		System.out.println("Toto");
+	}
+	
+	public static void displayTata() {
+		System.out.println("Tata");
 	}
 }
